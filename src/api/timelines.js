5ca32ea53841fd520c13f7199c7a1c761441cc19
Benoît Zugmeyer <bzugmeyer@gmail.com>
2017-04-23 23:23:43 +0200

    yay streaming!

--- src/api/timelines.js
+++ src/api/timelines.js
@@ -1,6 +1,6 @@
 import { requestWithToken } from "src/api/request"
 
-export async function getTimelinesHome(token) {
-    const response = await requestWithToken(token, "timelines/home")
+export async function getTimeline(token, type) {
+    const response = await requestWithToken(token, `timelines/${type}`)
     return await response.json()
 }
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,6 @@
 import { Component, h } from "preact"
-import { getTimelinesHome } from "src/api/timelines"
+import { getTimeline } from "src/api/timelines"
+import { subscribe } from "src/api/streaming"
 import Toot from "src/components/Toot"
 import style from "src/style"
 
@@ -11,15 +12,36 @@ const ss = style.namespace("Timeline").addRules({
     },
 })
 
+function mergeToots(timeline, toots) {
+    if (!timeline) return toots
+    const ids = new Set()
+    for (const toot of timeline) ids.add(toot.id)
+    const newTimeline = []
+    for (const toot of toots) {
+        if (!ids.has(toot.id)) newTimeline.push(toot)
+    }
+    newTimeline.push.apply(newTimeline, timeline)
+    return newTimeline
+}
+
 export default class Timeline extends Component {
     componentWillMount() {
+        this.unsubscribe = subscribe(this.props.token, this.props.type, (toot) => {
+            this.setState(({ timeline }) => ({
+                timeline: mergeToots(timeline, [toot]),
+            }))
+        })
         this.loadTimeline()
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe()
     }
 
     async loadTimeline() {
         this.setState({ loading: true })
-        const timeline = await getTimelinesHome(this.props.token)
-        this.setState({ loading: false, timeline })
+        const toots = await getTimeline(this.props.token, this.props.type)
+        this.setState(({timeline}) => ({ loading: false, timeline: mergeToots(timeline, toots) }))
     }
 
     render({ type }, { loading, timeline }) {
